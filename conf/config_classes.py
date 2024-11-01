144598799b045de14c259f23ee6b93a1cc002b9d
Dmitriy Antropov <dmitrymp3@gmail.com>
2024-11-01 12:22:31 +0300

    service fix

--- conf/config_classes.py
+++ conf/config_classes.py
@@ -22,60 +22,60 @@ class BotoConfig:
         """
         return {k: v for k, v in self.__dict__.items() if k != 's3_bucket' and v}
 
-@dataclass
-class DatabaseForBackup:
-    """
-    Класс описывает базу данных, ее название и количество хранимых бэкапов
-    """
-    name: str
-    temp_freq: InitVar[dict] = dict()
-    frequency: dict = field(default_factory=dict)
+# @dataclass
+# class DatabaseForBackup:
+#     """
+#     Класс описывает базу данных, ее название и количество хранимых бэкапов
+#     """
+#     name: str
+#     temp_freq: InitVar[dict] = dict()
+#     frequency: dict = field(default_factory=dict)
 
-    def __post_init__(self, temp_freq: dict):
-        # TODO: Надо как-то перетащить значения по умолчанию в конфиг
-        default_values = {
-            'DAILY'     : 4,
-            'WEEKLY'    : 4,
-            'MONTHLY'   : 4,
-            'YEARLY'    : 999,
-        }
-        # Такой метод выбран, чтобы отсечь неправильную передачу параметра, например, DILY, WEKLY
-        for key in default_values.keys():
-            self.frequency.update({key: temp_freq.get(key, default_values.get(key))})
+#     def __post_init__(self, temp_freq: dict):
+#         # TODO: Надо как-то перетащить значения по умолчанию в конфиг
+#         default_values = {
+#             'DAILY'     : 4,
+#             'WEEKLY'    : 4,
+#             'MONTHLY'   : 4,
+#             'YEARLY'    : 999,
+#         }
+#         # Такой метод выбран, чтобы отсечь неправильную передачу параметра, например, DILY, WEKLY
+#         for key in default_values.keys():
+#             self.frequency.update({key: temp_freq.get(key, default_values.get(key))})
 
 
-class DataBasesList:
-    """
-    Получает все базы данных и устанавливает им частоту ротации
-    Пример заполнения:
-    databases = DataBasesList({
-        '95c': {
-            'key': 'value', # случайные значения. Будут проигнорированы
-            'DAILY': 40
-            },
-        'volleyball_db': {}
-    })
-    """
-    db_dict: dict
+# class DataBasesList:
+#     """
+#     Получает все базы данных и устанавливает им частоту ротации
+#     Пример заполнения:
+#     databases = DataBasesList({
+#         '95c': {
+#             'key': 'value', # случайные значения. Будут проигнорированы
+#             'DAILY': 40
+#             },
+#         'volleyball_db': {}
+#     })
+#     """
+#     db_dict: dict
 
-    _default_values = {
-            'DAILY'     : 4,
-            'WEEKLY'    : 4,
-            'MONTHLY'   : 4,
-            'YEARLY'    : 999,
-        }
+#     _default_values = {
+#             'DAILY'     : 4,
+#             'WEEKLY'    : 4,
+#             'MONTHLY'   : 4,
+#             'YEARLY'    : 999,
+#         }
     
-    def __init__(self, databases: dict[str: dict]):
-        self.db_dict = {name: self.frequency_validator(freq) for name, freq in databases.items()}
+#     def __init__(self, databases: dict[str: dict]):
+#         self.db_dict = {name: self.frequency_validator(freq) for name, freq in databases.items()}
 
-    @staticmethod
-    def frequency_validator(freq: dict):
-        freq = filter(lambda x: x == isinstance())
-        return {k: freq.get(k, default) for k, default in __class__._default_values.items()}
+#     @staticmethod
+#     def frequency_validator(freq: dict):
+#         freq = filter(lambda x: x == isinstance())
+#         return {k: freq.get(k, default) for k, default in __class__._default_values.items()}
     
-    @property
-    def db_names(self):
-        return [db_name for db_name in self.db_dict.keys()]
+#     @property
+#     def db_names(self):
+#         return [db_name for db_name in self.db_dict.keys()]
     
 
 class AllDatabases:
